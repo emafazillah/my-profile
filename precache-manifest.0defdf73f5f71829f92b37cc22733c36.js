7b12b085a88fd96fcb50ac65b610763d0618209f
emafazillah <gatsu2811@gmail.com>
2020-07-05 09:15:11 +0100

    Updates

---
+++ precache-manifest.0defdf73f5f71829f92b37cc22733c36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983c572e9b10138791237792c6752eff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1ee3e3f929669232bb0",
+    "url": "/static/css/main.282f008a.chunk.css"
+  },
+  {
+    "revision": "5b1610ef19553f2e4c87",
+    "url": "/static/js/2.83bc250a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.83bc250a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ee3e3f929669232bb0",
+    "url": "/static/js/main.d2854af5.chunk.js"
+  },
+  {
+    "revision": "1ba0dcb417b8676ec9fd",
+    "url": "/static/js/runtime-main.1c823b01.js"
+  },
+  {
+    "revision": "a2d142c5d9b5b9c1e17208a61e01738d",
+    "url": "/static/media/octocat.a2d142c5.png"
+  }
+]);
